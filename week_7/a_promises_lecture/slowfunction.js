cfcb95ffb0e9d8c70226e7357599d9de39439563
Bishwajit Das <bishwajitkuat@gmail.com>
2023-03-10 10:29:48 +0200

    week_7

--- week_7/a_promises_lecture/slowfunction.js
+++ week_7/a_promises_lecture/slowfunction.js
@@ -1,21 +1,21 @@
-"use strict";
+'use strict'
 
 function slowRandomLengthOperation() {
   return new Promise((resolve, reject) => {
-    setTimeout(() => resolve("resolved"), Math.random() * 2000);
-    setTimeout(() => reject("rejected"), Math.random() * 2000);
-  });
+    setTimeout(() => resolve('resolved'), Math.random() * 2000)
+    setTimeout(() => reject('rejected'), Math.random() * 2000)
+  })
 }
 
 // slowRandomLengthOperation().then(console.log).catch(console.log);
 
 async function test() {
   try {
-    const result = await slowRandomLengthOperation();
-    console.log("resolved:", result);
-    console.log("something");
+    const result = await slowRandomLengthOperation()
+    console.log('resolved:', result)
+    console.log('something')
   } catch (err) {
-    console.log("error:", err);
+    console.log('error:', err)
   }
 }
 
@@ -23,9 +23,9 @@ async function test() {
 // Promise.all() => resolve if all promises are resolved
 Promise.all([slowRandomLengthOperation(), slowRandomLengthOperation()])
   .then(console.log)
-  .catch(console.log);
+  .catch(console.log)
 
 // Promise.any() => resolve if any prosie is resolved
 Promise.any([slowRandomLengthOperation(), slowRandomLengthOperation()])
   .then(console.log)
-  .catch(console.log);
+  .catch(console.log)
